bae73b294046a2588c0aeaa40f5430289381f090
Kasi David <kasidkure@gmail.com>
2024-09-26 17:54:05 +0100

    Wrote some nice code; looking tested and trusted...sigh, so cringe

--- tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
+++ tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
@@ -23,5 +23,36 @@ public class TextTruncatorTest {
     }
 
     /* ADD YOUR TESTS HERE */
+    @Test
+    public void truncateTo_receivesNullString_returnsEmptyString() {
+        String original = null;
+        TextTruncator truncator = new TextTruncator(original);
+
+        String truncatedString = truncator.truncateTo(10);
+
+        Assertions.assertEquals("", truncatedString,"Expected to return an empty string: '' ");
+    }
+
+    @Test
+    public void truncateTo_stringLongerThanNumChars_shortensToDesiredLength() {
+        String original = "I want to truncate you as nicely as possible.";
+        TextTruncator truncator = new TextTruncator(original);
+
+        String truncatedString = truncator.truncateTo(9);
+
+        Assertions.assertEquals(9,truncatedString.length(), "Expected truncated length to be 8 characters long");
+
+    }
+
+    @Test
+    public void truncateTo_stringShorterThanNumChars_doesNotAppendSuffix() {
+        String original = "I am short enough; don't touch me!";
+        TextTruncator truncator = new TextTruncator(original);
+
+        String truncatedString = truncator.truncateTo(34, "!");
+
+        Assertions.assertEquals(original, truncatedString, "Expected the string to remain untouched");
+    }
+
 
 }
